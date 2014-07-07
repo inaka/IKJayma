f1e218e16e9d897dcdd94bbe769f4d7db2dd043a
andres@inaka.com <andres.gerace@inakanetworks.com>
2014-07-07 17:15:42 -0300

    Documentation removed

--- IKJayma/Documentation/html/search/functions_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['writedocumentwithresponseobject_3a',['writeDocumentWithResponseObject:',['../interface_i_j_abstract_repository.html#a549ff3d651249e23fce04b4ddf34f7da',1,'IJAbstractRepository']]]
-];
