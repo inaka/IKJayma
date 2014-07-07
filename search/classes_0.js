d2fe7095f331212e21f6a8ba5e6119e1ff19efcd
andres@inaka.com <andres.gerace@inakanetworks.com>
2014-07-07 17:33:52 -0300

    Gera Added website documentaion

---
+++ search/classes_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['ijabstractdocument',['IJAbstractDocument',['../interface_i_j_abstract_document.html',1,'']]],
+  ['ijabstractrepository',['IJAbstractRepository',['../interface_i_j_abstract_repository.html',1,'']]],
+  ['ijafnetworkingbackend',['IJAFNetworkingBackend',['../interface_i_j_a_f_networking_backend.html',1,'']]],
+  ['ijerror',['IJError',['../interface_i_j_error.html',1,'']]]
+];
---
+++ search/properties_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentid',['documentId',['../interface_i_j_abstract_document.html#aa2696bc058879761be4214d033e54870',1,'IJAbstractDocument']]]
+];
