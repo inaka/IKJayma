727fde7a2769ce1f782b66219b64deae91c06ac4
andres@inaka.com <andres.gerace@inakanetworks.com>
2014-07-14 11:18:06 -0300

    fixes #4 added main page documentation

--- IKJayma/IKJayma/IJAbstractBackend.h
+++ IKJayma/IKJayma/IJAbstractBackend.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "IJError.h"
 @interface IJAbstractBackend : NSObject
+/* The queue that handles the NSOperations */
+@property (strong) NSOperationQueue * operationsQueue;
+/*! \brief ABSTRACT METHOD. You'll need to write your custom implementation.
+ *
+ *   This will create and return an NSOperation.
+ */
+- (NSOperation *)operationWithRequest:(NSURLRequest *)request success:(void (^)(NSOperation *operation, id responseObject))success failure:(void (^)(IJError *error))failure;
+/*! \brief ABSTRACT METHOD. You'll need to write your custom implementation.
+ *
+ *   This will add a request to a queue so you can stop all operations in case you don't need them anymore. In case you need to implement a not-contemplated method in your repositories you'll need to send your request here. Other way you won't never use it.
+ */
+- (void)queueRequest:(NSURLRequest *)request success:(void (^)(NSOperation *operation, id responseObject))success failure:(void (^)(IJError *error))failure;
 
 @end
