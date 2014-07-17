ce0810db9ea10522c5f205bcd2f9692578c68cef
andres@inaka.com <andres.gerace@inakanetworks.com>
2014-07-17 17:33:08 -0300

    Gera.finish to fix the merge problems fixing the IJAbstractBackend and IJAFNetworkingBackend

--- IKJayma/IKJayma/IJAbstractBackend.m
+++ IKJayma/IKJayma/IJAbstractBackend.m
@@ -9,5 +9,24 @@
 #import "IJAbstractBackend.h"
 
 @implementation IJAbstractBackend
+- (id) init
+{
+    if (self = [super init])
+    {
+        self.operationsQueue = [[NSOperationQueue alloc]init];
+    }
+    return self;
+}
 
+- (NSOperation *)operationWithRequest:(NSURLRequest *)request success:(void (^)(NSOperation *operation, id responseObject))success failure:(void (^)(IJError *error))failure
+{
+    NSAssert(NO, @"This is an abstract method and should be overridden");
+    return nil;
+}
+
+- (void)queueRequest:(NSURLRequest *)request success:(void (^)(NSOperation *operation, id responseObject))success failure:(void (^)(IJError *error))failure
+{
+    NSOperation *operation = [self operationWithRequest:request success:success failure: failure];
+    [self.operationsQueue addOperation:operation];
+}
 @end
